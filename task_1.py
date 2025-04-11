ad1e372932d755a4cca522080db3c1bd1969a4a5
Escala-diaz <zaidm7074@gmail.com>
2025-04-12 03:20:42 +0530

    Create task_1.py

---
+++ task_1.py
@@ -0,0 +1,14 @@
+first=input("Enter First Number:")
+first=int(first)
+second=input("Enter Second Number:")
+second=int(second)
+
+add= first + second
+sub= first - second
+multiply= first * second
+divide= first / second
+
+print("Addition: ", add)
+print("Subtraction: ", sub)
+print("Multiplication: ", multiply)
+print("Division: ", divide)
